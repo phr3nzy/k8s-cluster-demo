d6704b13594cf92a03e5c2657018c1f1cec6c143
phr3nzy <adilosama47@gmail.com>
2020-01-10 11:58:23 +0200

    Edit: route all request to /api

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 
 const { SERVER_PORT = 3000 } = process.env;
 
-app.use(routes);
+app.use("/api", routes);
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Listening on port ${SERVER_PORT}`);
